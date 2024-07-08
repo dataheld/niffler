d45654a68df38b242f0fd2063f48516aafe5fbb8
Maximilian Held <info@maxheld.de>
2024-07-08 22:43:08 +0200

    add abort helper

---
+++ R/modules.R
@@ -0,0 +1,17 @@
+#' Helper to stop if input is or is not a reactive
+#' @inheritParams shiny::is.reactive
+#' @keywords module helpers
+#' @export
+abort_if_reactive <- function(x) {
+  if (shiny::is.reactive(x)) {
+    rlang::abort("Input must not be reactive.")
+  }
+}
+
+#' @rdname abort_if_reactive
+#' @export
+abort_if_not_reactive <- function(x) {
+  if (!shiny::is.reactive(x)) {
+    rlang::abort("Input must be reactive.")
+  }
+}
